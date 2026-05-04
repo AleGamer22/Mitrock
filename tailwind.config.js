c8cf7f34a88a46f13f46af6ea2d147d019d3661a
AleNationVibes <58488998+AleGamer22@users.noreply.github.com>
2026-05-04 12:56:14 +0200

    updt

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.{html,js}", "./static/js/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
